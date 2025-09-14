dcd5065d27e39db28fab4c51177a3e5d1349dbe6
razalcp <cprazalnazim@gmail.com>
2025-09-14 12:02:05 +0530

    dist_css

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // Tailwind CSS
-import AppRouter from "./src/App"; // import your router
+import "./index.css";
+import AppRouter from "./src/App"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(AppRouter);
