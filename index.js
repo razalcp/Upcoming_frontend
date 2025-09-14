f592ea3587619046ce3e617f4e97c11ec0346a03
razalcp <cprazalnazim@gmail.com>
2025-09-14 12:37:24 +0530

    new_change

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRouter from "./src/App"; // Your App.js
+import "./index.css"; // Tailwind CSS
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(AppRouter);
